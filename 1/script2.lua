8e7e3d10739481d095b9b4e2b26a0512b69983cf
NoFunJack <64732115+NoFunJack@users.noreply.github.com>
2021-12-01 08:12:15 +0100

    simpler solution

--- 1/script2.lua
+++ 1/script2.lua
@@ -8,69 +8,32 @@ function inputnumbers()
 end
 
 
-Buffer = {}
-function Buffer:new ()
-  o = {bufferPos = 0}   -- create object if user does not provide one
-  setmetatable(o, self)
-  self.__index = self
-  return o
-end
+Buffer = {bufferPos = 0}
 
 function Buffer:addToBuffer(n) 
   self[self.bufferPos] = n
-  self.bufferPos = self.bufferPos + 1
+  self.bufferPos = (self.bufferPos + 1) % 3
 end
 
 function Buffer:sum() 
   return self[0] + self[1] + self[2]
 end
 
-function buildInc()
-  local last 
-  local bufferPos = 0
-
-
-  return function(n)  
-    local re
-    if last and last < n then 
-      re =  1
-    else 
-      re = 0
-    end
-    last = n
-    return re
-  end
-end
-
-local isInc = buildInc()
 local sumInc = 0
-local buffList = {}
 
-function feedBuffers(i,n) 
-  buffList[i] = Buffer:new()
-
-  incBuffNr(i-2,n)
-  incBuffNr(i-1,n)
-  incBuffNr(i-0,n)
-  
-  --del buffer
-  buffList[i-3] = nil
-end
-
-function incBuffNr(idx,n) 
-  if buffList[idx] then
-    buffList[idx]:addToBuffer(n)
-  end
-end
 
 for i,n in inputnumbers() do
 
-  feedBuffers(i,n)
-
-  if buffList[i-2] then
-    local incVal = isInc(buffList[i-2]:sum())
-    --io.write(n," ", incVal ,"\n")
-    sumInc = sumInc + incVal
+  if i > 3 then
+    --io.write("\n",n)
+    local oldVal = Buffer:sum()
+    Buffer:addToBuffer(n)
+    if Buffer:sum() > oldVal then
+      --io.write(" inc")
+      sumInc = sumInc + 1
+    end
+   else
+    Buffer:addToBuffer(n)
   end
 end
 
