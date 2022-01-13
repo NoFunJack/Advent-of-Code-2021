3f6e2d11046e56501c291c2e96f86cae74b2063a
NoFunJack <64732115+NoFunJack@users.noreply.github.com>
2022-01-13 17:37:03 +0100

    day 20 solved

--- 20/lib/image.py
+++ 20/lib/image.py
@@ -2,6 +2,8 @@ class Image:
     def __init__(self,mapstr,algoArr):
         self.__parse_mapstr(mapstr)
         self.algoArr = algoArr
+        self.__update_bounds()
+        self.out_bounds_state = False
 
     def __parse_mapstr(self,mapstr):
         lines = mapstr.split("\n")
@@ -14,12 +16,16 @@ class Image:
         pass
 
     def enhance(self):
+
         n = []
-        for x in range(self.__min_pos(0)-1,self.__max_pos(0)+2):
-            for y in range(self.__min_pos(1)-1,self.__max_pos(1)+2):
+        for x in range(self.x_min-1,self.x_max+2):
+            for y in range(self.y_min-1,self.y_max+2):
                 if self.__getsig(x,y) in self.algoArr:
                     n.append((x,y))
         self.map=n
+        self.__inc_bounds()
+        if 0 in self.algoArr:
+            self.out_bounds_state = not self.out_bounds_state
 
     def __getsig(self,x,y):
         bitstr =""
@@ -28,21 +34,50 @@ class Image:
                 if (x+i,y+j) in self.map:
                     bitstr += "1"
                 else:
-                    bitstr += "0"
+                    if self.__is_out_of_bounds(x+i,y+j):
+                        if self.out_bounds_state:
+                            #print("oob",x+i,y+j,self.out_bounds_state)
+                            bitstr += "1"
+                        else:
+                            bitstr += "0"
+                    else:
+                        bitstr += "0"
 
+        #print(x,y,"\t",bitstr)
         return int(bitstr,2)
 
+    def __update_bounds(self):
+        self.x_min= self.__min_pos(0)
+        self.y_min= self.__min_pos(1)
+        self.x_max= self.__max_pos(0)
+        self.y_max= self.__max_pos(1)
+
+    def __inc_bounds(self):
+        self.x_min -= 1
+        self.y_min -= 1
+        self.x_max += 1
+        self.y_max += 1
+
+    def __is_out_of_bounds(self,x,y):
+        return x < self.x_min or x > self.x_max or y < self.y_min or y > self.y_max
 
     def __min_pos(self,dim):
-        return min(self.map,key=lambda v: v[dim])[dim];
+        if self.map:
+            return min(self.map,key=lambda v: v[dim])[dim];
+        else:
+            return 0
         
     def __max_pos(self,dim):
-        return max(self.map,key=lambda v: v[dim])[dim];
+        if self.map:
+            return max(self.map,key=lambda v: v[dim])[dim];
+        else:
+            return 0
 
     def __str__(self):
-        s = ""
-        for y in range(self.__min_pos(1)-1,self.__max_pos(1)+2):
-            for x in range(self.__min_pos(0)-1,self.__max_pos(0)+2):
+        s = f"out of bounds state: {self.out_bounds_state}\n"
+        s += f"bounds({self.x_min},{self.x_max},{self.y_min},{self.y_max})\n\n"
+        for y in range(self.y_min,self.y_max+1):
+            for x in range(self.x_min,self.x_max+1):
                 if (x,y) in self.map:
                     s += '#'
                 else:
@@ -50,3 +85,4 @@ class Image:
             s += "\n"
 
         return s
+
--- 20/tests/test_image.py
+++ 20/tests/test_image.py
@@ -5,28 +5,94 @@ from lib.image import Image
 
 class ImageTests(unittest.TestCase):
 
-    def create00(self,algo):
-        i = Image("#",algo);
-        self.assertEqual(i.map,[(0,0)])
-        return i
+  def create00(self,algo):
+      i = Image("#",algo);
+      self.assertEqual(i.map,[(0,0)])
+      return i
 
-    def test_delete_everything(self):
-        i = self.create00([])
-        i.enhance()
-        self.assertEqual(i.map,[])
+  def test_delete_everything(self):
+      i = self.create00([])
+      i.enhance()
+      self.assertEqual(i.map,[])
 
-    def test_move_up(self):
-        i = self.create00([int('000_000_010',2)])
-        i.enhance()
-        self.assertEqual(i.map,[(0,-1)])
+  def test_move_up(self):
+      i = self.create00([int('000_000_010',2)])
+      i.enhance()
+      self.assertEqual(i.map,[(0,-1)])
 
-    def test_multiply(self):
-        i = self.create00(
-                [int('000_000_001',2),
-                int('000_000_010',2),
-                int('000_000_100',2)]
-                )
-        i.enhance()
-        self.assertCountEqual(i.map,[(1,-1),(0,-1),(-1,-1)])
-        i.enhance()
-        self.assertCountEqual(i.map,[(2,-2),(-2,-2)])
+  def test_multiply(self):
+      i = self.create00(
+              [int('000_000_001',2),
+              int('000_000_010',2),
+              int('000_000_100',2)]
+              )
+      i.enhance()
+      self.assertCountEqual(i.map,[(1,-1),(0,-1),(-1,-1)])
+      i.enhance()
+      self.assertCountEqual(i.map,[(2,-2),(-2,-2)])
+
+  def test_nothing_is_something(self):
+      i = self.create00([
+          0,
+          int('111_000_111',2),
+          int('000_010_000',2),
+          ])
+      i.enhance()
+      self.assertTrue(i.out_bounds_state)
+      self.assertEqual(len(i.map),1)
+      i.enhance()
+      self.assertEqual(len(i.map),1)
+
+  def test_nothing_is_and_more(self):
+      i = self.create00([
+          0,
+          int('111_000_010',2),
+          int('000_010_000',2),
+          ])
+      print(i)
+      i.enhance()
+      print(i)
+      self.assertTrue(i.out_bounds_state)
+      self.assertEqual(len(i.map),1)
+      i.enhance()
+      print(i)
+      self.assertEqual(len(i.map),2)
+
+  def test_always_new_stuff_incorner(self):
+      i = self.create00([
+          0,
+          int('111_100_100',2),
+          int('111_100_101',2),
+          int('000_010_000',2),
+          ])
+      i.enhance()
+      self.assertTrue(i.out_bounds_state)
+      self.assertEqual(len(i.map),1)
+      i.enhance()
+      self.assertEqual(len(i.map),2)
+      i.enhance()
+      self.assertEqual(len(i.map),2)
+
+  def test_dent(self):
+      i = Image(
+"""
+#...#
+#####
+"""
+          ,[
+          0,
+          int('000_000_111',2),
+          ])
+      i.enhance()
+      self.assertTrue(i.out_bounds_state)
+      self.assertEqual(len(i.map),2)
+      i.enhance()
+      self.assertEqual(len(i.map),0)
+
+  def test_blink_blank(self):
+      i = Image('',[i for i in range(0,511)])
+      self.assertFalse(int('111_111_111',2) in i.algoArr) 
+      i.enhance()
+      self.assertTrue(i.out_bounds_state)
+      i.enhance()
+      self.assertEqual(len(i.map),0)
