9bb27e8ca6477438f4ed175241e003e5bb32ff80
NoFunJack <64732115+NoFunJack@users.noreply.github.com>
2022-01-22 11:48:32 +0100

    day 22 done messy

--- 22/app.py
+++ 22/app.py
@@ -6,26 +6,58 @@ import numpy as np
 
 class OffsetCube:
     def __init__(self,bounds):
-        self.offset = (bounds[0],bounds[2],bounds[4])
-        self.upper_edge = (bounds[1],bounds[3],bounds[5])
-        off = [
-            self.upper_edge[0]-self.offset[0]+1,
-            self.upper_edge[1]-self.offset[1]+1,
-            self.upper_edge[2]-self.offset[2]+1,
-        ]
-        self.cube = np.ones(off,dtype=np.int8)
+        self.bounds = bounds
 
-    def substact(self,bounds):
-        low,up = self.subcube_of(bounds)
-        if low:
-            self.cube[low[0]:up[0]+1,low[1]:up[1]+1,low[2]:up[2]+1] = 0
+    def cut(self,bounds):
+        old_lights = self.volume()
+        re = []
+        skip = False
+        #print("\ncut",self.bounds,bounds)
+        for i in range(0,6,2):
+            new_cube = self.bounds[:]
+            if bounds[i] > self.bounds[i]:
+                #print("cut low",i)
+                b = min(bounds[i]-1,self.bounds[i+1])
+                new_cube[i+1] = b
+                re.append(OffsetCube(new_cube))
+                if b == self.bounds[i+1]:
+                    #print("whole cube deleted",b)
+                    skip = True
+                    break
+                self.bounds[i] = max(b+1,self.bounds[i])
+                #print("new cube",new_cube,b)
+                #print("remainder",self.bounds)
+        if not skip:
+            for i in range(1,6,2):
+                new_cube = self.bounds[:]
+                if bounds[i] < self.bounds[i]:
+                    #print("cut high",i)
+                    b = max(bounds[i]+1,self.bounds[i-1])
+                    new_cube[i-1] = b
+                    re.append(OffsetCube(new_cube))
+                    if b == self.bounds[i-1]:
+                        #print("whole cube deleted",b)
+                        break
+                    self.bounds[i] = min(b-1,self.bounds[i])
+                    #print("new cube",new_cube,b)
+                    #print("remainder",self.bounds)
+                
+        #print("new cubes",[c.bounds for c in re])
+        diff = sum([off.volume() for off in re])-old_lights
+        #print("diff:",diff)
+        for c in re:
+            assert c.volume() > 0
+        assert diff<=0
 
-    def subcube_of(self,global_bounds):
-        low_edge = [max(global_bounds[i*2],self.offset[i]) for i in range(0,3)]
-        up_edge = [min(global_bounds[i*2+1],self.upper_edge[i]) for i in range(0,3)]
-        low_edge = [low_edge[i]-self.offset[i] for i in range(0,3)]
-        up_edge = [up_edge[i]-self.offset[i] for i in range(0,3)]
-        return low_edge,up_edge
+        return re
+
+
+    def volume(self):
+        dim = [self.bounds[i*2+1]-self.bounds[i*2]+1 for i in range(0,3)]
+        re = np.prod(dim)
+        ##print(f"{self.bounds} dim: {dim} has volume of {re}")
+        return re
+
 
 
 # Try to use numpy for the On-blocks
@@ -33,32 +65,38 @@ on_cubes = []
 
 
 def add_cubes(bounds):
+    global on_cubes
+    new_cubes = []
     for c in on_cubes:
-        c.substact(bounds) 
+        new_cubes.extend(c.cut(bounds))
+    on_cubes = new_cubes
     on_cubes.append(OffsetCube(bounds))
 
 def remove_cubes(bounds):
+    global on_cubes
+    new_cubes = []
     for c in on_cubes:
-        c.substact(bounds) 
+        new_cubes.extend(c.cut(bounds))
+    on_cubes = new_cubes
 
 print("booting reactor\n")
 p = re.compile("(\w+) x=(-?\d+)..(-?\d+),y=(-?\d+)..(-?\d+),z=(-?\d+)..(-?\d+)")
 with open(sys.argv[1], 'r') as file:
-    for line in file:
+    for lineNum,line in enumerate(file):
         line =line.rstrip()
-        print(line)
+        print(lineNum,line)
         m = p.match(line)
         bounds = [int(n) for n in m.groups()[1:7]]
         if m.group(1) == "on":
             add_cubes(bounds)
         else:
             remove_cubes(bounds)
-        print(sum([off.cube.sum() for off in on_cubes]))
+        print("~~lights:",sum([off.volume() for off in on_cubes]))
 
 
 
 
 print("final lights",)
-print(sum([off.cube.sum() for off in on_cubes]))
+print(sum([off.volume() for off in on_cubes]))
 
 
