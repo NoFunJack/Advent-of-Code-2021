e7229954891f04b93e03a784b6245e58a52eb11e
NoFunJack <64732115+NoFunJack@users.noreply.github.com>
2022-06-06 14:41:25 +0200

    23: move into room

--- 23/src/main.rs
+++ 23/src/main.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 fn main() {
     let hw = Hallway::new(
         [
@@ -51,26 +52,29 @@ impl Hallway {
         Hallway {
             content: [None; 11],
             rooms: [
-                Room::new(Pod::A, rooms[0][0], rooms[0][1]),
-                Room::new(Pod::B, rooms[1][0], rooms[1][1]),
-                Room::new(Pod::C, rooms[2][0], rooms[2][1]),
-                Room::new(Pod::D, rooms[3][0], rooms[3][1]),
+                Room::new(rooms[0][0], rooms[0][1]),
+                Room::new(rooms[1][0], rooms[1][1]),
+                Room::new(rooms[2][0], rooms[2][1]),
+                Room::new(rooms[3][0], rooms[3][1]),
             ],
             energy,
         }
     }
 
     pub fn get_children(&self) -> Vec<Hallway> {
-        self.move_to_hw()
+        let mut re = Vec::new();
+        re.append(&mut self.move_to_hw());
+        re.append(&mut self.move_to_room());
+        re
     }
 
     pub fn is_done(&self) -> bool {
         self.rooms
             == [
-                Room::new(Pod::A, Some(Pod::A), Some(Pod::A)),
-                Room::new(Pod::B, Some(Pod::B), Some(Pod::B)),
-                Room::new(Pod::C, Some(Pod::C), Some(Pod::C)),
-                Room::new(Pod::D, Some(Pod::D), Some(Pod::D)),
+                Room::new(Some(Pod::A), Some(Pod::A)),
+                Room::new(Some(Pod::B), Some(Pod::B)),
+                Room::new(Some(Pod::C), Some(Pod::C)),
+                Room::new(Some(Pod::D), Some(Pod::D)),
             ]
     }
 
@@ -78,7 +82,7 @@ impl Hallway {
         let mut re = Vec::new();
         for (i, _) in self.rooms.iter().enumerate() {
             let mut clone = self.clone();
-            if let Some((pod, cost)) = clone.rooms[i].get() {
+            if let Some((pod, cost)) = clone.rooms[i].get(Hallway::room_dest(i)) {
                 clone.energy += cost;
                 re.append(&mut move_pod_lr(clone, pod, ROOM_POS[i]));
             }
@@ -131,19 +135,70 @@ impl Hallway {
 
         re
     }
+
+    fn move_to_room(&self) -> Vec<Hallway> {
+        let mut re = Vec::new();
+        for (i, tile) in self.content.iter().enumerate() {
+            if let Some(pod) = tile {
+                if let Ok(to_room_cost) = self.pod_can_go_to_room(pod, i) {
+                    let mut clone = self.clone();
+                    clone.content[i] = None;
+                    let room = &mut clone.rooms[Hallway::room_idx(*pod)];
+                    if let Some(cost) = room.add(*pod).ok() {
+                        clone.energy += cost + to_room_cost;
+                        re.push(clone);
+                    }
+                }
+            }
+        }
+        re
+    }
+
+    fn pod_can_go_to_room(&self, pod: &Pod, from: usize) -> Result<usize, ()> {
+        let door_idx = ROOM_POS[Hallway::room_idx(*pod)];
+        let range = match from.cmp(&door_idx) {
+            Ordering::Less => (from + 1)..door_idx,
+            Ordering::Equal => 0..0,
+            Ordering::Greater => door_idx..from,
+        };
+
+        for i in range {
+            if let Some(_) = self.content[i] {
+                return Err(());
+            }
+        }
+        Ok(from.abs_diff(door_idx))
+    }
+
+    fn room_idx(pod: Pod) -> usize {
+        match pod {
+            Pod::A => 0,
+            Pod::B => 1,
+            Pod::C => 2,
+            Pod::D => 3,
+        }
+    }
+
+    fn room_dest(i: usize) -> Pod {
+        match i {
+            0 => Pod::A,
+            1 => Pod::B,
+            2 => Pod::C,
+            3 => Pod::D,
+            _ => panic!("unknown room number"),
+        }
+    }
 }
 
 #[derive(Debug, Clone, PartialEq)]
 struct Room {
-    dest_for: Pod,
     content: [Option<Pod>; 2],
 }
 
 impl Room {
-    fn new(dest_for: Pod, upper: Option<Pod>, lower: Option<Pod>) -> Room {
+    fn new(upper: Option<Pod>, lower: Option<Pod>) -> Room {
         Room {
             content: [upper, lower],
-            dest_for,
         }
     }
 
@@ -164,8 +219,8 @@ impl Room {
         }
     }
 
-    pub fn get(&mut self) -> Option<(Pod, usize)> {
-        if self.all_pods_done() {
+    pub fn get(&mut self, ok_pod: Pod) -> Option<(Pod, usize)> {
+        if self.all_pods_done(ok_pod) {
             return None;
         }
 
@@ -178,11 +233,11 @@ impl Room {
         }
     }
 
-    fn all_pods_done(&self) -> bool {
+    fn all_pods_done(&self, dest: Pod) -> bool {
         self.content
             .iter()
             .filter(|t| t.is_some())
-            .all(|p| p.unwrap() == self.dest_for)
+            .all(|p| p.unwrap() == dest)
     }
 }
 
@@ -220,7 +275,7 @@ mod tests {
 
     #[test]
     fn two_pods_per_room() {
-        let mut room = Room::new(Pod::A, None, None);
+        let mut room = Room::new(None, None);
 
         // move 2 fields deep
         let first_insert = room.add(Pod::A);
@@ -237,24 +292,22 @@ mod tests {
 
     #[test]
     fn get_pods_from_room() {
-        let mut room = Room::new(Pod::A, Some(Pod::A), Some(Pod::B));
-        assert_eq!(room.get().unwrap(), (Pod::A, 1));
-        assert_eq!(room.get().unwrap(), (Pod::B, 20));
-        assert_eq!(room.get(), None);
+        let mut room = Room::new(Some(Pod::A), Some(Pod::B));
+        assert_eq!(room.get(Pod::C).unwrap(), (Pod::A, 1));
+        assert_eq!(room.get(Pod::C).unwrap(), (Pod::B, 20));
+        assert_eq!(room.get(Pod::C), None);
     }
 
     #[test]
-    fn do_not_take_out_finished_pod() {
-        let mut room = Room::new(Pod::A, None, Some(Pod::A));
-        assert_eq!(room.get(), None);
-        let mut room = Room::new(Pod::A, Some(Pod::A), Some(Pod::A));
-        assert_eq!(room.get(), None);
+    fn if_all_pods_ok_dont_give_1() {
+        let mut room = Room::new(None, Some(Pod::A));
+        assert_eq!(room.get(Pod::A), None);
     }
 
     #[test]
     fn dont_block_other_non_test_pod() {
-        let mut room = Room::new(Pod::A, Some(Pod::A), Some(Pod::B));
-        assert_eq!(room.get(), Some((Pod::A, 1)));
+        let mut room = Room::new(Some(Pod::A), Some(Pod::B));
+        assert_eq!(room.get(Pod::C), Some((Pod::A, 1)));
     }
 
     #[test]
@@ -327,5 +380,66 @@ mod tests {
         }
 
         assert_eq!(hw.move_to_hw().len(), 0)
+    }
+
+    #[test]
+    fn move_last_to_room() {
+        let hw = Hallway {
+            energy: 0,
+            content: [
+                Some(Pod::A),
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+            ],
+            rooms: [
+                Room::new(Some(Pod::A), None),
+                Room::new(Some(Pod::B), Some(Pod::B)),
+                Room::new(Some(Pod::C), Some(Pod::C)),
+                Room::new(Some(Pod::D), Some(Pod::D)),
+            ],
+        };
+
+        let children = hw.get_children();
+        assert_eq!(children.len(), 1);
+        assert_eq!(children[0].energy, 3);
+        assert!(children[0].is_done());
+    }
+
+    #[test]
+    fn move_last_to_room_blocked() {
+        let hw = Hallway {
+            energy: 0,
+            content: [
+                Some(Pod::D),
+                None,
+                None,
+                Some(Pod::A),
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+            ],
+            rooms: [
+                Room::new(Some(Pod::A), Some(Pod::A)),
+                Room::new(Some(Pod::B), Some(Pod::B)),
+                Room::new(Some(Pod::C), Some(Pod::C)),
+                Room::new(Some(Pod::D), None),
+            ],
+        };
+
+        let children = hw.get_children();
+        assert!(!hw.is_done());
+        assert_eq!(children.len(), 0);
     }
 }
