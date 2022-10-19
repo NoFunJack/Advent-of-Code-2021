0e10d56c22fab12f04ec1f09ab484faad35e1a21
NoFunJack <64732115+NoFunJack@users.noreply.github.com>
2022-10-19 16:29:53 +0200

    day 10 part2:w

--- 10/src/lib.rs
+++ 10/src/lib.rs
@@ -1,16 +1,27 @@
-pub fn check(line: String) -> usize {
+pub fn check(line: String) -> CheckResult {
     let mut stack = Stack::new();
 
     for c in line.chars() {
         match stack.push(c) {
             Ok(_) => {}
-            Err(score) => return score,
+            Err(score) => return CheckResult::Corrupted(score),
         }
     }
 
-    0
+    println!("DEBUG {:#?}", stack);
+    CheckResult::Inclomplete(
+        stack.autocomplete_score(),
+        stack.expected_closer.iter().rev().collect(),
+    )
 }
 
+#[derive(PartialEq, Debug)]
+pub enum CheckResult {
+    Corrupted(usize),
+    Inclomplete(usize, String),
+}
+
+#[derive(Debug)]
 struct Stack {
     expected_closer: Vec<char>,
 }
@@ -34,23 +45,42 @@ impl Stack {
                         return Ok(());
                     } else {
                         println!("expected {:?} but found {}", self.expected_closer.last(), c);
-                        Err(score_of(c))
+                        Err(error_score_of(c))
                     }
                 } else {
                     println!("expected {:?} but found {}", self.expected_closer.last(), c);
-                    Err(score_of(c))
+                    Err(error_score_of(c))
                 }
             }
         }
     }
+
+    fn autocomplete_score(&self) -> usize {
+        let mut score = 0;
+
+        for c in self.expected_closer.iter().rev() {
+            score = (5 * score) + autocomplete_score_of(*c);
+        }
+
+        score
+    }
 }
 
-fn score_of(c: char) -> usize {
+fn error_score_of(c: char) -> usize {
     match c {
         ')' => 3,
         ']' => 57,
         '}' => 1197,
         '>' => 25137,
+        _ => panic!("Unknown char {}", c),
+    }
+}
+fn autocomplete_score_of(c: char) -> usize {
+    match c {
+        ')' => 1,
+        ']' => 2,
+        '}' => 3,
+        '>' => 4,
         _ => panic!("Unknown char {}", c),
     }
 }
@@ -77,36 +107,123 @@ mod test {
 
     #[test]
     fn legal_input_no_points() {
-        assert_eq!(check("()".to_string()), 0);
-        assert_eq!(check("[]".to_string()), 0);
-        assert_eq!(check("([])".to_string()), 0);
-        assert_eq!(check("{()()()}".to_string()), 0);
-        assert_eq!(check("<([{}])>".to_string()), 0);
-        assert_eq!(check("[<>({}){}[([])<>]]".to_string()), 0);
-        assert_eq!(check("(((((((((())))))))))".to_string()), 0);
+        assert_eq!(
+            check("()".to_string()),
+            CheckResult::Inclomplete(0, "".to_string())
+        );
+        assert_eq!(
+            check("[]".to_string()),
+            CheckResult::Inclomplete(0, "".to_string())
+        );
+        assert_eq!(
+            check("([])".to_string()),
+            CheckResult::Inclomplete(0, "".to_string())
+        );
+        assert_eq!(
+            check("{()()()}".to_string()),
+            CheckResult::Inclomplete(0, "".to_string())
+        );
+        assert_eq!(
+            check("<([{}])>".to_string()),
+            CheckResult::Inclomplete(0, "".to_string())
+        );
+        assert_eq!(
+            check("[<>({}){}[([])<>]]".to_string()),
+            CheckResult::Inclomplete(0, "".to_string())
+        );
+        assert_eq!(
+            check("(((((((((())))))))))".to_string()),
+            CheckResult::Inclomplete(0, "".to_string())
+        );
+    }
+    #[test]
+    fn incomplete_is_ok() {
+        //assert_eq!(check("[".to_string()), build_incl("]", 2));
+        //assert_eq!(check("<<[<>]".to_string()), build_incl(">>", 24));
+        //assert_eq!(check("{{{[{}]".to_string()), build_incl("}}}", 93));
+        //assert_eq!(check("([{<".to_string()), build_incl(">}])", 194));
+        //assert_eq!(check("(<()<>>".to_string()), build_incl(")", 1));
+        assert_eq!(
+            check("[({([[{{".to_string()),
+            build_incl("}}]])})]", 288957)
+        );
+
+        fn build_incl(exp_end: &str, score: usize) -> CheckResult {
+            CheckResult::Inclomplete(score, exp_end.to_string())
+        }
     }
 
     #[test]
     fn simple_examples() {
-        assert_eq!(check("(]".to_string()), score_of(']'));
-        assert_eq!(check("{()()()>".to_string()), score_of('>'));
-        assert_eq!(check("(((()))}".to_string()), score_of('}'));
-        assert_eq!(check("<([]){()}[{}])".to_string()), score_of(')'));
-    }
+        assert_eq!(
+            check("(]".to_string()),
+            CheckResult::Corrupted(error_score_of(']'))
+        );
+        assert_eq!(
+            check("{()()()>".to_string()),
+            CheckResult::Corrupted(error_score_of('>'))
+        );
+        assert_eq!(
+            check("(((()))}".to_string()),
+            CheckResult::Corrupted(error_score_of('}'))
+        );
 
-    #[test]
-    fn incomplete_is_ok() {
-        assert_eq!(check("[".to_string()), 0);
-        assert_eq!(check("<<[<>]".to_string()), 0);
-        assert_eq!(check("{{{[{}]".to_string()), 0);
+        assert_eq!(
+            check("<([]){()}[{}])".to_string()),
+            CheckResult::Corrupted(error_score_of(')'))
+        );
     }
 
     #[test]
     fn complex_examples() {
-        assert_eq!(check("{([(<{}[<>[]}>{[]{[(<()>".to_string()), score_of('}'));
-        assert_eq!(check("[[<[([]))<([[{}[[()]]]".to_string()), score_of(')'));
-        assert_eq!(check("[{[{({}]{}}([{[{{{}}([]".to_string()), score_of(']'));
-        assert_eq!(check("[<(<(<(<{}))><([]([]()".to_string()), score_of(')'));
-        assert_eq!(check("<{([([[(<>()){}]>(<<{{".to_string()), score_of('>'));
+        assert_error(
+            check("{([(<{}[<>[]}>{[]{[(<()>".to_string()),
+            error_score_of('}'),
+        );
+        assert_error(
+            check("[[<[([]))<([[{}[[()]]]".to_string()),
+            error_score_of(')'),
+        );
+        assert_error(
+            check("[{[{({}]{}}([{[{{{}}([]".to_string()),
+            error_score_of(']'),
+        );
+        assert_error(
+            check("[<(<(<(<{}))><([]([]()".to_string()),
+            error_score_of(')'),
+        );
+        assert_error(
+            check("<{([([[(<>()){}]>(<<{{".to_string()),
+            error_score_of('>'),
+        );
+
+        fn assert_error(result: CheckResult, score: usize) {
+            match result {
+                CheckResult::Corrupted(s) => assert_eq!(s, score),
+                _ => panic!("Line not corrupted"),
+            }
+        }
+    }
+
+    #[test]
+    fn empty_ac_score() {
+        let s = Stack::new();
+        assert_eq!(s.autocomplete_score(), 0);
+    }
+
+    #[test]
+    fn ac_score_examples() {
+        assert_eq!(call_score_calc("}}]])})]"), 288957);
+        assert_eq!(call_score_calc(")}>]})"), 5566);
+        assert_eq!(call_score_calc("}}>}>))))"), 1480781);
+        assert_eq!(call_score_calc("]]}}]}]}>"), 995444);
+        assert_eq!(call_score_calc("])}>"), 294);
+    }
+    fn call_score_calc(input: &str) -> usize {
+        let s = Stack {
+            expected_closer: input.chars().rev().collect(),
+        };
+
+        return s.autocomplete_score();
     }
 }
--- 10/src/main.rs
+++ 10/src/main.rs
@@ -10,14 +10,28 @@ fn main() {
     println!("{:?}", args);
 
     let stdin = io::stdin();
-    let mut sum = 0;
+    let mut sum_corrupted = 0;
+    let mut list_autocomplete = Vec::new();
 
     for line in stdin.lock().lines() {
         let result = check(line.as_ref().unwrap().clone());
-        println!("input {} score {}", line.unwrap(), result);
-        sum += result;
+        println!("input {} score {:?}", line.unwrap(), result);
+        match result {
+            lib::CheckResult::Corrupted(s) => sum_corrupted += s,
+            lib::CheckResult::Inclomplete(s, _) => list_autocomplete.push(s),
+        }
     }
 
-    println!("Final sum: {}", sum);
+    list_autocomplete.sort();
+    println!("Final sum corrupted: {}", sum_corrupted);
+    println!(
+        "DEBUG: {}, {} ",
+        list_autocomplete.len(),
+        list_autocomplete.len() / 2
+    );
+    println!(
+        "Final middle autocomplete: {:#?}\n {}",
+        list_autocomplete,
+        list_autocomplete.get(list_autocomplete.len() / 2).unwrap()
+    );
 }
-
