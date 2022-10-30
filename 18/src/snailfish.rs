4325f6ffd1cd0594e7a56aa716284ea0c920838f
NoFunJack <64732115+NoFunJack@users.noreply.github.com>
2022-10-30 14:10:20 +0100

    simplfied snail numbers

--- 18/src/snailfish.rs
+++ 18/src/snailfish.rs
@@ -1,14 +1,9 @@
-use std::{fmt::Debug, str::Chars, usize};
+use std::{cell::RefCell, fmt::Debug, rc::Rc, str::Chars, usize};
+use Number::{Pair, Value};
 
 #[derive(PartialEq, Eq)]
-pub struct Number {
-    left: Box<Content>,
-    right: Box<Content>,
-}
-
-#[derive(PartialEq, Eq)]
-pub enum Content {
-    Pair(Number),
+pub enum Number {
+    Pair(Rc<RefCell<Number>>, Rc<RefCell<Number>>),
     Value(isize),
 }
 
@@ -17,100 +12,47 @@ impl Number {
         build(&mut s.chars(), NumberBuilderState::Init)
     }
 
-    fn new(left: Content, right: Content) -> Number {
-        Number {
-            left: Box::new(left),
-            right: Box::new(right),
-        }
+    fn new(left: Number, right: Number) -> Number {
+        Pair(Rc::new(RefCell::new(left)), Rc::new(RefCell::new(right)))
     }
 
-    fn get_values(&self) -> (isize, isize) {
-        let l = match *self.left {
-            Content::Pair(..) => panic!(),
-            Content::Value(v) => v,
-        };
-        let r = match *self.right {
-            Content::Pair(..) => panic!(),
-            Content::Value(v) => v,
-        };
-
-        (l, r)
+    fn leafs_mut(start: Rc<RefCell<Number>>) -> SnailNumberIter {
+        SnailNumberIter::from(start)
     }
 
-    fn explode_deep_numbers(&mut self) {
+    fn explode_first_deep_number(&mut self) {
         self.explode_int(0);
     }
 
     fn explode_int(&mut self, depth: usize) -> (Option<isize>, Option<isize>) {
-        let mut re = (None, None);
-
-        println!("Debug d:{} num:{:?}", depth, self);
-
-        if depth == 3 {
-            println!("exp num: {:?}", self);
-            if let Content::Pair(p) = &*self.left {
-                let (l, r) = p.get_values();
-                re = (Some(l), None);
-                match &*self.right {
-                    Content::Pair(np) => panic!("exploding left has pair right {:?}", np),
-                    Content::Value(v) => {
-                        self.right = Box::new(Content::Value(v + r));
-                        self.left = Box::new(Content::Value(0));
-                    }
-                }
-            }
-            if let Content::Pair(p) = &*self.right {
-                let (l, r) = p.get_values();
-                re = (None, Some(r));
-                match &*self.left {
-                    Content::Pair(np) => panic!("exploding right has pair left {:?}", np),
-                    Content::Value(v) => {
-                        self.left = Box::new(Content::Value(v + l));
-                        self.right = Box::new(Content::Value(0));
-                    }
-                }
-            }
-        } else {
-            if let Content::Pair(p) = &mut *self.left {
-                re = p.explode_int(depth + 1);
-            }
-            if let Content::Pair(p) = &mut *self.right {
-                re = p.explode_int(depth + 1);
-            }
-            println!("re {:?}", re);
-
-            if let Some(l) = re.0 {
-                if let Content::Value(v) = *self.left {
-                    println!("moved value {} to {}", l, v);
-                    self.left = Box::new(Content::Value(v + l));
-                    re.0 = None;
-                }
-            }
-            if let Some(r) = re.1 {
-                if let Content::Value(v) = *self.right {
-                    println!("moved value {} to {}", r, v);
-                    self.right = Box::new(Content::Value(v + r));
-                    re.1 = None;
-                }
-            }
-        }
-
-        re
+        todo!();
     }
 }
 
 impl Debug for Number {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Number: [{:?},{:?}]", &self.left, &self.right)
+        match self {
+            Self::Pair(l, r) => write!(f, "[{:?},{:?}]", l, r),
+            Self::Value(v) => write!(f, "{}", v),
+        }
     }
 }
 
-impl Debug for Content {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::Pair(p) => write!(f, "[{:?},{:?}]", p.left, p.right),
-            Self::Value(v) => write!(f, "{}", v),
-        }
+struct SnailNumberIter {
+    current: Rc<RefCell<Number>>,
+}
+
+impl SnailNumberIter {
+    fn from(root: Rc<RefCell<Number>>) -> SnailNumberIter {
+        SnailNumberIter { current: root }
+    }
+}
+
+impl Iterator for SnailNumberIter {
+    type Item = Rc<Number>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        todo!()
     }
 }
 
@@ -129,17 +71,17 @@ fn build(iter: &mut Chars, mut state: NumberBuilderState) -> Number {
             '[' => match state {
                 NumberBuilderState::Init => state = NumberBuilderState::InBracket,
                 NumberBuilderState::InBracket => {
-                    left = Some(Content::Pair(build(iter, NumberBuilderState::InBracket)))
+                    left = Some(build(iter, NumberBuilderState::InBracket))
                 }
                 NumberBuilderState::AfterComma => {
-                    right = Some(Content::Pair(build(iter, NumberBuilderState::InBracket)))
+                    right = Some(build(iter, NumberBuilderState::InBracket))
                 }
             },
             '0'..='9' => {
                 if left.is_none() {
-                    left = Some(Content::Value(c.to_digit(10).unwrap().try_into().unwrap()));
+                    left = Some(Number::Value(c.to_digit(10).unwrap().try_into().unwrap()));
                 } else {
-                    right = Some(Content::Value(c.to_digit(10).unwrap().try_into().unwrap()));
+                    right = Some(Number::Value(c.to_digit(10).unwrap().try_into().unwrap()));
                 }
             }
             ',' => state = NumberBuilderState::AfterComma,
@@ -153,38 +95,36 @@ fn build(iter: &mut Chars, mut state: NumberBuilderState) -> Number {
 #[cfg(test)]
 mod test {
 
+    use super::Number::Pair;
+    use super::Number::Value;
     use super::*;
 
     #[test]
     fn test_input_simple() {
         let n = phase_str("[1,2]");
-        assert!(matches!(*n.left, Content::Value(1)));
-        assert!(matches!(*n.right, Content::Value(2)));
+        assert_eq!(n, Number::new(Value(1), Value(2)));
     }
 
     #[test]
     fn test_input_left_nested() {
         let n = phase_str("[[1,3],2]");
-        match *n.left {
-            Content::Pair(sub) => {
-                assert!(matches!(*sub.left, Content::Value(1)));
-                assert!(matches!(*sub.right, Content::Value(3)));
+        match n {
+            Pair(left, right) => {
+                assert_eq!(*left.borrow(), Number::new(Value(1), Value(3)));
+                assert_eq!(*right.borrow(), Value(2));
             }
             _ => panic!("left is not a pair"),
         }
-
-        assert!(matches!(*n.right, Content::Value(2)));
     }
 
     #[test]
     fn test_input_right_nested() {
         let n = phase_str("[1,[3,2]]");
 
-        assert!(matches!(*n.left, Content::Value(1)));
-        match *n.right {
-            Content::Pair(sub) => {
-                assert!(matches!(*sub.left, Content::Value(3)));
-                assert!(matches!(*sub.right, Content::Value(2)));
+        match n {
+            Pair(left, right) => {
+                assert_eq!(*left.borrow(), Value(1));
+                assert_eq!(*right.borrow(), Number::new(Value(3), Value(2)));
             }
             _ => panic!("left is not a pair"),
         }
@@ -207,7 +147,7 @@ mod test {
     fn explode_deep_nested_4() {
         assert_explotion(
             "[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]",
-            "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]",
+            "[[3,[2,[8,0]]],[9,[5,[7,[3,2]]]]]",
         );
     }
     #[test]
@@ -218,10 +158,18 @@ mod test {
         );
     }
 
+    #[test]
+    fn leaf_iter() {
+        let n = phase_str("[1,0]");
+        let mut iter = Number::leafs_mut(Rc::new(RefCell::new(n)));
+        assert_eq!(iter.next(), Some(Rc::new(phase_str("[1,0]"))));
+        assert_eq!(iter.next(), None);
+    }
+
     fn assert_explotion(n: &str, expected: &str) {
         let mut n = phase_str(n);
         let expected_after = phase_str(expected);
-        n.explode_deep_numbers();
+        n.explode_first_deep_number();
         assert_eq!(n, expected_after)
     }
 
